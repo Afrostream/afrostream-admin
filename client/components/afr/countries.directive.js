e9c9e659e9bdfb395cbf1d1fc85fbd19a91f1fa7
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-10-25 14:41:36 +0200

    feature: 22 new countries

--- client/components/afr/countries.directive.js
+++ client/components/afr/countries.directive.js
@@ -7,18 +7,44 @@ angular.module('afrostreamAdminApp')
     // FIXME: this list should be loaded from the api.
     // main code
     scope.countries = [
-      { _id: 'BE', name: 'Belgique' },
-      { _id: 'CH', name: 'Suisse' },
-      { _id: 'CI', name: 'Côté d\'Ivoire' },
+      // FR
       { _id: 'FR', name: 'France' },
+      // outre mer
       { _id: 'GF', name: 'Guyane française' },
       { _id: 'GP', name: 'Guadeloupe' },
-      { _id: 'LU', name: 'Luxembourg' },
       { _id: 'MF', name: 'Saint Martin' },
       { _id: 'MQ', name: 'Martinique' },
       { _id: 'PF', name: 'Polynésie française' },
       { _id: 'RE', name: 'Réunion' },
-      { _id: 'SN', name: 'Sénégal' }
+      // EU
+      { _id: 'BE', name: 'Belgique' },
+      { _id: 'CH', name: 'Suisse' },
+      { _id: 'LU', name: 'Luxembourg' },
+      // Afrique
+      { _id: 'BF', name: 'Burkina Faso' },
+      { _id: 'BI', name: 'Burundi' },
+      { _id: 'BJ', name: 'Benin' },
+      { _id: 'CD', name: 'Congo, The Democratic Republic of the' },
+      { _id: 'CF', name: 'Central African Republic' },
+      { _id: 'CG', name: 'Congo' },
+      { _id: 'CI', name: 'Côté d\'Ivoire' },
+      { _id: 'CM', name: 'Cameroon' },
+      { _id: 'CV', name: 'Cape Verde' },
+      { _id: 'DJ', name: 'Djibouti' },
+      { _id: 'GA', name: 'Gabon' },
+      { _id: 'GN', name: 'Guinea' },
+      { _id: 'GQ', name: 'Equatorial Guinea' },
+      { _id: 'GW', name: 'Guinea-Bissau' },
+      { _id: 'KM', name: 'Comoros' },
+      { _id: 'MG', name: 'Madagascar' },
+      { _id: 'ML', name: 'Mali' },
+      { _id: 'MR', name: 'Mauritania' },
+      { _id: 'MU', name: 'Mauritius' },
+      { _id: 'NE', name: 'Niger' },
+      { _id: 'SC', name: 'Seychelles' },
+      { _id: 'SN', name: 'Sénégal' },
+      { _id: 'TD', name: 'Chad' },
+      { _id: 'TG', name: 'Togo' }
     ];
     scope.toggle = function (country) {
       var p = scope.props.countries.indexOf(country._id);
