2ee99c3685f131ecea1376277e79f79729ad1c3e
benjipott <pott.benjamin@gmail.com>
2016-11-02 17:21:04 +0100

    data list error

--- client/app/press/press.controller.js
+++ client/app/press/press.controller.js
@@ -1,12 +1,5 @@
 'use strict';
 
 angular.module('afrostreamAdminApp')
-  .controller('PressCtrl', function ($scope, Press) {
-    $scope.loadPress = function (query) {
-      var p = Press.query({query: query}).$promise;
-      p.then(function (response) {
-        return response;
-      });
-      return p;
-    };
+  .controller('PressCtrl', function ($scope) {
   });
