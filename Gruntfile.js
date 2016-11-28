76b45e893d42e0cb7811f5117f1568d6db6f464c
benjipott <pott.benjamin@gmail.com>
2016-11-28 07:08:20 +0100

    fix order jquery

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
       target: {
         src: '<%= yeoman.client %>/index.html',
         ignorePath: '<%= yeoman.client %>/',
-        exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/]
+        exclude: [/jquery/, /jquery-ui/, /bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/]
       }
     },
 
