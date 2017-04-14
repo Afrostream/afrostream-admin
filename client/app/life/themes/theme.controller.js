26949c045e2e68a5093b2f23f67c37e1f62a1209
benjipott <pott.benjamin@gmail.com>
2017-04-14 14:56:12 +0200

    fix tags

--- client/app/life/themes/theme.controller.js
+++ client/app/life/themes/theme.controller.js
@@ -2,6 +2,23 @@
 
 angular.module('afrostreamAdminApp')
   .controller('LifeThemeCtrl', function ($scope, LifePin) {
+
+    $scope.$watch('item.keywords', function (keywords) {
+      $scope.tags = [];
+      angular.forEach(keywords, function (key, value) {
+        $scope.tags.push({
+          text: value
+        })
+      });
+    }, true);
+
+    $scope.$watch('tags', function (tags) {
+      $scope.item.keywords = [];
+      for (var key in tags) {
+        $scope.item.keywords.push(tags[key].text);
+      }
+    }, true);
+
     $scope.loadLifePin = function (query) {
       return LifePin.query({query: query}).$promise;
     };
