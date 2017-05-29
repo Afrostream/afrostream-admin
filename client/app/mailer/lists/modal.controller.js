6681d49332bc950d075c1357566bca61356648f1
Marc Dassonneville <marc.dassonneville@gmail.com>
2017-05-29 14:51:31 +0200

    mailer: gui: loading provider list

--- client/app/mailer/lists/modal.controller.js
+++ client/app/mailer/lists/modal.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('afrostreamAdminApp')
-  .controller('MailerListModalCtrl', function ($scope, item, type, ngToast, $timeout, $uibModalInstance, ModalUtils) {
+  .controller('MailerListModalCtrl', function ($scope, $http, item, type, ngToast, $timeout, $uibModalInstance, ModalUtils) {
     $scope.modalType = type;
     $scope.modalHooks = {};
 
@@ -31,11 +31,18 @@ angular.module('afrostreamAdminApp')
 
     $scope.getItem = ModalUtils.createHandlerGetItem($scope, item);
 
-
-
     // getItem is often triggered before modal hooks are binded (directives aren't yet loaded)
     //  so this timeout is a quick fix & not the proper way of mitigating the pb
     $timeout(function () {
       $scope.getItem();
     }, 100);
+
+
+    /////////////////////////// specific code ///////////////
+    $scope.loading = true;
+
+    $http.get('/api/mailer/providers')
+      .then(function (result) {
+        $scope.providers = result.data;
+      });
   });
